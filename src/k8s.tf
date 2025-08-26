df279e5ff13fb6a81438ea617ca98bee28951fd7
felipelaptrin <paotrindadepao@gmail.com>
2025-08-26 10:54:15 -0300

    add external secrets

--- src/k8s.tf
+++ src/k8s.tf
@@ -61,3 +61,24 @@ module "external_dns_pod_identity" {
     }
   }
 }
+
+##############################
+##### EXTERNAL SECRETS
+##############################
+module "external_secrets_pod_identity" {
+  source  = "terraform-aws-modules/eks-pod-identity/aws"
+  version = "v1.11.0"
+
+  name = "external-secrets"
+
+  attach_external_secrets_policy        = true
+  external_secrets_secrets_manager_arns = ["arn:aws:secretsmanager:${var.aws_region}:*:*:*"]
+
+  associations = {
+    external-secrets = {
+      cluster_name    = module.eks.cluster_name
+      namespace       = "external-secrets"
+      service_account = "external-secrets"
+    }
+  }
+}
